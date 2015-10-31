2a52a3b6264b44091f81f9da89239b2a3fdb3899
ericgottschalk <you@example.com>
2015-10-31 20:37:54 -0200

    Adicionar ModuloEleitor

---
+++ UrnaApi/UrnaApi.Dominio/ModuloEleitor/Eleitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrnaApi.Dominio.Comum;
+
+namespace UrnaApi.Dominio.ModuloEleitor
+{
+    public class Eleitor : EntidadeBase
+    {
+        public string Nome { get; set; }
+
+        public string TituloEleitor { get; set; }
+
+        public string RG { get; set; }
+
+        public string CPF { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string ZonaEleitoral { get; set; }
+
+        public string Secao { get; set; }
+
+        public char Situacao { get; set; }
+
+        public bool Votou { get; set; }
+    }
+}
---
+++ UrnaApi/UrnaApi.Dominio/ModuloEleitor/IRepositorioEleitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrnaApi.Dominio.Comum;
+
+namespace UrnaApi.Dominio.ModuloEleitor
+{
+    public interface IRepositorioEleitor : IRepositorio<Eleitor>
+    {
+        void Excluir(Eleitor eleitor);
+    }
+}
