305fdc578c1494d37e56343d4c789cc311f85001
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-02 03:18:43 -0200

    adicionado classe Voto

---
+++ UrnaApi/UrnaApi.Dominio/ModuloVoto/Voto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrnaApi.Dominio.ModuloVoto
+{
+    public class Voto
+    {
+        public int IDVoto { get; set; }
+        public int IDCandidato { get; set; }
+    }
+}
