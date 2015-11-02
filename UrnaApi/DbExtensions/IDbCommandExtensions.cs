b42ec02776c4eb7eca8440084411eb779c5d6903
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-01 22:39:05 -0200

    adicionado AddParameter

---
+++ UrnaApi/DbExtensions/IDbCommandExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbExtensions
+{
+    public static class IDbCommandExtensions
+    {
+        public static void AddParameter(this IDbCommand idb,string nome,object valor)
+        {
+            IDbDataParameter param = idb.CreateParameter();
+            param.ParameterName = nome;
+            param.Value = valor ?? DBNull.Value;
+            idb.Parameters.Add(param);
+        }
+    }
+}
