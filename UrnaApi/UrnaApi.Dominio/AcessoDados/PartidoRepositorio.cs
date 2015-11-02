9e7809b6b0dff3c7cd332f82145e7a81300d8811
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-02 02:14:57 -0200

    implementado excluir e cadastrar partido

--- UrnaApi/UrnaApi.Dominio/AcessoDados/PartidoRepositorio.cs
+++ UrnaApi/UrnaApi.Dominio/AcessoDados/PartidoRepositorio.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using UrnaApi.Dominio.ModuloPartido;
 using DbExtensions;
+using System.Transactions;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace UrnaApi.Dominio.AcessoDados
 {
@@ -12,12 +16,47 @@ namespace UrnaApi.Dominio.AcessoDados
     {
         public void Excluir(Partido partido)
         {
-            throw new NotImplementedException();
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+            using (TransactionScope transacao = new TransactionScope())
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                IDbCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "DELETE FROM Partido WHERE IDPartido = @paramIDPartido";
+                cmd.AddParameter("paramIDPartido", partido.Id);
+
+                connection.Open();
+
+                cmd.ExecuteNonQuery();
+
+                transacao.Complete();
+                connection.Close();
+            }
         }
 
         public void Cadastrar(Partido item)
         {
-            throw new NotImplementedException();
+            List<Partido> partidoEncontrado = FindByName(item.Nome);
+            if (partidoEncontrado.TrueForAll(partido=>partido.Nome == item.Nome) && partidoEncontrado.TrueForAll(partido=>partido.Slogan == item.Slogan))
+            {
+                throw new Exception("Nome e slogan ja existentes.");
+            }
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+            using (TransactionScope transacao = new TransactionScope())
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                IDbCommand comando = connection.CreateCommand();
+                comando.CommandText =
+                    "INSERT INTO Partido (Nome,Slogan,Sigla) VALUES (@paramNome,@paramSlogan,@paramSigla)";
+                comando.AddParameter("paramNome", item.Nome);
+                comando.AddParameter("paramSlogan", item.Slogan);
+                comando.AddParameter("paramSigla", item.Sigla);
+                connection.Open();
+
+                comando.ExecuteNonQuery();
+
+                transacao.Complete();
+                connection.Close();
+            }
         }
 
         public void Editar(Partido item)
