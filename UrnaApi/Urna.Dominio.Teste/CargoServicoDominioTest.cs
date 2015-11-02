648eaf35d9c0e2600fb256fd522babf8aa9ea8c6
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-02 15:06:49 -0200

    implementado teste do Cargo

---
+++ UrnaApi/Urna.Dominio.Teste/CargoServicoDominioTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrnaApi.Dominio.ModuloCargo;
+using UrnaApi.Dominio.AcessoDados;
+
+namespace Urna.Dominio.Teste
+{
+    [TestClass]
+    public class CargoServicoDominioTest
+    {
+        [TestMethod]
+        public void Cadastrar()
+        {
+            ICargoRepositorio repositorio = new CargoRepositorio();
+            var servico = new CargoServicoDominio(repositorio);
+
+            Cargo cargo = new Cargo()
+            {
+                Nome = "Cargo Test",
+                Situacao = 'A'              
+            };
+
+            servico.Add(cargo);
+
+            Assert.IsTrue(servico.FindByName("Cargo Tes").Count > 0);
+        }
+
+        [TestMethod]
+        public void Editar()
+        {
+            ICargoRepositorio repositorio = new CargoRepositorio();
+            var servico = new CargoServicoDominio(repositorio);
+
+            Cargo cargo = new Cargo()
+            {
+                Nome = "Editar Test",
+                Situacao = 'A'
+            };
+
+            servico.Add(cargo);
+            var tempCandidato = servico.FindByName("Editar T")[0];
+
+            tempCandidato.Nome = "Editado com sucesso";
+            servico.Editar(tempCandidato);
+
+            Assert.IsTrue(servico.FindByName("Editado").Count > 0);
+        }
+    }
+}
