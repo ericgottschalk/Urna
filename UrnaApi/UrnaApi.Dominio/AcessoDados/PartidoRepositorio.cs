1556cfb9a173bf96e10bc31ff753446eecef01d9
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-02 02:23:52 -0200

    implementado FindById

--- UrnaApi/UrnaApi.Dominio/AcessoDados/PartidoRepositorio.cs
+++ UrnaApi/UrnaApi.Dominio/AcessoDados/PartidoRepositorio.cs
@@ -66,7 +66,38 @@ namespace UrnaApi.Dominio.AcessoDados
 
         public Partido FindById(int id)
         {
-            throw new NotImplementedException();
+            Partido partidoEncontrado = null;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                IDbCommand comando = connection.CreateCommand();
+                comando.CommandText =
+                    "SELECT IDPartido,Nome,Slogan,Sigla FROM Cargo WHERE IDPartido = @paramIDPartido";
+
+                comando.AddParameter("paramIDPartido", id);
+
+                connection.Open();
+                IDataReader reader = comando.ExecuteReader();
+                if (reader.Read())
+                {
+                    int idDb = Convert.ToInt32(reader["IDPartido"]);
+                    string nome = reader["Nome"].ToString();
+                    string slogan = reader["Slogan"].ToString();
+                    string sigla = reader["Sigla"].ToString();
+
+                    partidoEncontrado = new Partido()
+                    {
+                        Id = idDb,
+                        Nome = nome,
+                        Slogan = slogan,
+                        Sigla = sigla
+                    };
+                }
+                connection.Close();
+            }
+
+            return partidoEncontrado;
         }
 
         public List<Partido> FindByName(string name)
